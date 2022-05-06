277b0c0dfb757d578b7b1a59c0153871eb07b979
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-05-05 22:00:57 -0500

    probando comit de otra maquina

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Primer commit Brajhan
+          Primer commit Brajhan  y yo
         </p>
         <a
           className="App-link"
